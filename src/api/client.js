863174f27df2cf48b5602a786b6f6cb65cc70622
chineduokotu <chineokotu@gmail.com>
2026-01-31 02:07:54 +0100

    added backend url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+const prod_url="https://chris-market-place-server.onrender.com";
+//const dev_url="http://localhost:8000";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || prod_url + '/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
